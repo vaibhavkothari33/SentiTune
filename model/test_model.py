1d3cc7ce6dfa900e81d5fe82caa8ee266733af03
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-09-19 11:17:32 +0530

    60% acc

--- model/test_model.py
+++ model/test_model.py
@@ -1,33 +1,3 @@
-# import pickle
-# import cv2
-# from utils import get_face_landmarks
-
-# emotions = ['ANGRY', 'DISGUSTED', 'FEARFUL',"hAPPY","NEUTRAL"]
-
-# with open('./model1', 'rb') as f:
-#     model = pickle.load(f)
-
-# cap = cv2.VideoCapture(0)
-# ret, frame = cap.read()
-
-# while ret:
-#     ret, frame = cap.read()
-
-#     face_landmarks = get_face_landmarks(frame, draw=True, static_image_mode=False)
-
-#     output = model.predict([face_landmarks])
-
-#     cv2.putText(frame,emotions[int(output[0])],(10, frame.shape[0] - 1),cv2.FONT_HERSHEY_SIMPLEX,3,(0, 255, 0),5)
-
-#     cv2.imshow('frame', frame)
-
-#     cv2.waitKey(25)
-
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-
 import pickle
 import cv2
 from utils import get_face_landmarks
@@ -36,7 +6,7 @@ from utils import get_face_landmarks
 emotions = ['ANGRY', 'DISGUSTED', 'FEARFUL', 'HAPPY', 'NEUTRAL']
 
 # Load the trained model from the file
-with open('./modelOne2.pkl', 'rb') as f:
+with open('./modelOne1.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Initialize the webcam
--- model/train_model.py
+++ model/train_model.py
@@ -13,15 +13,11 @@ X = data[:, :-1]  # Features are all columns except the last one
 y = data[:, -1]   # Labels are the last column
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42,shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42,shuffle=True)
 
-# Initialize the Random Forest Classifier
 rf_classifier = RandomForestClassifier()
-
-# Train the classifier on the training data
 rf_classifier.fit(X_train, y_train)
 
-# Make predictions on the test data
 y_pred = rf_classifier.predict(X_test)
 
 # Evaluate the accuracy of the model
@@ -29,56 +25,8 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy * 100:.2f}%")
 print(confusion_matrix(y_test, y_pred))
 
-    # with open('./modelOne', 'wb') as f:
-    #     pickle.dump(rf_classifier, f)
-
-
-# import pickle
-# import numpy as np
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score, confusion_matrix
-
-# # Initialize empty lists to store features (X) and labels (y)
-# all_X = []
-# all_y = []
-
-# # Load data from the three batch files and accumulate
-# # for i in range(1, 4):  # Assuming files are named data_part1.txt, data_part2.txt, data_part3.txt
-# data_file = f"./data_part_one.txt"
-# data = np.loadtxt(data_file)
-    
-#     # Separate features (X) and labels (y)
-#     X = data[:, :-1]  # Features are all columns except the last one
-#     y = data[:, -1]   # Labels are the last column
-    
-#     # Accumulate the data from all batches
-#     all_X.append(X)
-#     all_y.append(y)
-
-# # # Concatenate all batches into a single dataset
-# # all_X = np.vstack(all_X)
-# # all_y = np.hstack(all_y)
-
-# # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42, shuffle=True)
-
-# # Initialize the Random Forest Classifier
-# rf_classifier = RandomForestClassifier()
-
-# # Train the classifier on the training data
-# rf_classifier.fit(X_train, y_train)
-
-# # Make predictions on the test data
-# y_pred = rf_classifier.predict(X_test)
-
-# # Evaluate the accuracy of the model
-# accuracy = accuracy_score(y_test, y_pred)
-# print(f"Accuracy: {accuracy * 100:.2f}%")
-# print(confusion_matrix(y_test, y_pred))
-
 # Save the trained model to a file
-with open('./modelOne2.pkl', 'wb') as f:
+with open('./modelOne3.pkl', 'wb') as f:
     pickle.dump(rf_classifier, f)
 
 print("Model saved as modelOne.pkl")
